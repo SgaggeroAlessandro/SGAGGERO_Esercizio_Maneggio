afd50ec8622e6ae2fda2f1a3b9f6e55dadf7e74c
SgaggeroAlessandro <14529841@itisrossi.vi.it>
2026-03-11 12:48:49 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio_Maneggio/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Esercizio_Maneggio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBNome = new System.Windows.Forms.TextBox();
+            this.txtBRazza = new System.Windows.Forms.TextBox();
+            this.txtBAnno = new System.Windows.Forms.TextBox();
+            this.rdBM = new System.Windows.Forms.RadioButton();
+            this.rdBF = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbCavalli = new System.Windows.Forms.ListBox();
+            this.btnSalva = new System.Windows.Forms.Button();
+            this.btnCambia = new System.Windows.Forms.Button();
+            this.btnCerca = new System.Windows.Forms.Button();
+            this.btnRimuovi = new System.Windows.Forms.Button();
+            this.pnlCerca = new System.Windows.Forms.Panel();
+            this.txtCerca = new System.Windows.Forms.TextBox();
+            this.btnCercaNome = new System.Windows.Forms.Button();
+            this.pnlCerca.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBNome
+            // 
+            this.txtBNome.Location = new System.Drawing.Point(12, 54);
+            this.txtBNome.Name = "txtBNome";
+            this.txtBNome.Size = new System.Drawing.Size(142, 22);
+            this.txtBNome.TabIndex = 0;
+            this.txtBNome.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBNome_KeyPress);
+            // 
+            // txtBRazza
+            // 
+            this.txtBRazza.Location = new System.Drawing.Point(228, 54);
+            this.txtBRazza.Name = "txtBRazza";
+            this.txtBRazza.Size = new System.Drawing.Size(143, 22);
+            this.txtBRazza.TabIndex = 1;
+            this.txtBRazza.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBRazza_KeyPress);
+            // 
+            // txtBAnno
+            // 
+            this.txtBAnno.Location = new System.Drawing.Point(424, 54);
+            this.txtBAnno.Name = "txtBAnno";
+            this.txtBAnno.Size = new System.Drawing.Size(123, 22);
+            this.txtBAnno.TabIndex = 2;
+            this.txtBAnno.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBAnno_KeyPress);
+            // 
+            // rdBM
+            // 
+            this.rdBM.AutoSize = true;
+            this.rdBM.Location = new System.Drawing.Point(575, 56);
+            this.rdBM.Name = "rdBM";
+            this.rdBM.Size = new System.Drawing.Size(89, 20);
+            this.rdBM.TabIndex = 3;
+            this.rdBM.TabStop = true;
+            this.rdBM.Text = "MASCHIO";
+            this.rdBM.UseVisualStyleBackColor = true;
+            this.rdBM.CheckedChanged += new System.EventHandler(this.rdBM_CheckedChanged);
+            // 
+            // rdBF
+            // 
+            this.rdBF.AutoSize = true;
+            this.rdBF.Location = new System.Drawing.Point(679, 54);
+            this.rdBF.Name = "rdBF";
+            this.rdBF.Size = new System.Drawing.Size(89, 20);
+            this.rdBF.TabIndex = 4;
+            this.rdBF.TabStop = true;
+            this.rdBF.Text = "FEMMINA";
+            this.rdBF.UseVisualStyleBackColor = true;
+            this.rdBF.CheckedChanged += new System.EventHandler(this.rdBF_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "NOME DEL CAVALLO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "RAZZA DEL CAVALLO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ANNO DI NASCITA:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(608, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "SESSO:";
+            // 
+            // lbCavalli
+            // 
+            this.lbCavalli.FormattingEnabled = true;
+            this.lbCavalli.ItemHeight = 16;
+            this.lbCavalli.Location = new System.Drawing.Point(15, 117);
+            this.lbCavalli.Name = "lbCavalli";
+            this.lbCavalli.Size = new System.Drawing.Size(913, 212);
+            this.lbCavalli.TabIndex = 12;
+            // 
+            // btnSalva
+            // 
+            this.btnSalva.Location = new System.Drawing.Point(15, 357);
+            this.btnSalva.Name = "btnSalva";
+            this.btnSalva.Size = new System.Drawing.Size(139, 23);
+            this.btnSalva.TabIndex = 13;
+            this.btnSalva.Text = "SALVA CAVALLO";
+            this.btnSalva.UseVisualStyleBackColor = true;
+            this.btnSalva.Click += new System.EventHandler(this.btnSalva_Click);
+            // 
+            // btnCambia
+            // 
+            this.btnCambia.Location = new System.Drawing.Point(187, 357);
+            this.btnCambia.Name = "btnCambia";
+            this.btnCambia.Size = new System.Drawing.Size(172, 23);
+            this.btnCambia.TabIndex = 14;
+            this.btnCambia.Text = "CAMBIA DATI CAVALLO ";
+            this.btnCambia.UseVisualStyleBackColor = true;
+            // 
+            // btnCerca
+            // 
+            this.btnCerca.Location = new System.Drawing.Point(380, 357);
+            this.btnCerca.Name = "btnCerca";
+            this.btnCerca.Size = new System.Drawing.Size(167, 23);
+            this.btnCerca.TabIndex = 15;
+            this.btnCerca.Text = "CERCA UN CAVALLO:";
+            this.btnCerca.UseVisualStyleBackColor = true;
+            this.btnCerca.Click += new System.EventHandler(this.btnCerca_Click);
+            // 
+            // btnRimuovi
+            // 
+            this.btnRimuovi.Location = new System.Drawing.Point(564, 357);
+            this.btnRimuovi.Name = "btnRimuovi";
+            this.btnRimuovi.Size = new System.Drawing.Size(150, 23);
+            this.btnRimuovi.TabIndex = 16;
+            this.btnRimuovi.Text = "RIMUOVI CAVALLO";
+            this.btnRimuovi.UseVisualStyleBackColor = true;
+            this.btnRimuovi.Click += new System.EventHandler(this.btnRimuovi_Click);
+            // 
+            // pnlCerca
+            // 
+            this.pnlCerca.Controls.Add(this.btnCercaNome);
+            this.pnlCerca.Controls.Add(this.txtCerca);
+            this.pnlCerca.Location = new System.Drawing.Point(380, 395);
+            this.pnlCerca.Name = "pnlCerca";
+            this.pnlCerca.Size = new System.Drawing.Size(167, 132);
+            this.pnlCerca.TabIndex = 18;
+            // 
+            // txtCerca
+            // 
+            this.txtCerca.Location = new System.Drawing.Point(13, 16);
+            this.txtCerca.Name = "txtCerca";
+            this.txtCerca.Size = new System.Drawing.Size(133, 22);
+            this.txtCerca.TabIndex = 0;
+            // 
+            // btnCercaNome
+            // 
+            this.btnCercaNome.Location = new System.Drawing.Point(44, 56);
+            this.btnCercaNome.Name = "btnCercaNome";
+            this.btnCercaNome.Size = new System.Drawing.Size(75, 23);
+            this.btnCercaNome.TabIndex = 1;
+            this.btnCercaNome.Text = "CERCA";
+            this.btnCercaNome.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 539);
+            this.Controls.Add(this.pnlCerca);
+            this.Controls.Add(this.btnRimuovi);
+            this.Controls.Add(this.btnCerca);
+            this.Controls.Add(this.btnCambia);
+            this.Controls.Add(this.btnSalva);
+            this.Controls.Add(this.lbCavalli);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdBF);
+            this.Controls.Add(this.rdBM);
+            this.Controls.Add(this.txtBAnno);
+            this.Controls.Add(this.txtBRazza);
+            this.Controls.Add(this.txtBNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlCerca.ResumeLayout(false);
+            this.pnlCerca.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBNome;
+        private System.Windows.Forms.TextBox txtBRazza;
+        private System.Windows.Forms.TextBox txtBAnno;
+        private System.Windows.Forms.RadioButton rdBM;
+        private System.Windows.Forms.RadioButton rdBF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lbCavalli;
+        private System.Windows.Forms.Button btnSalva;
+        private System.Windows.Forms.Button btnCambia;
+        private System.Windows.Forms.Button btnCerca;
+        private System.Windows.Forms.Button btnRimuovi;
+        private System.Windows.Forms.Panel pnlCerca;
+        private System.Windows.Forms.Button btnCercaNome;
+        private System.Windows.Forms.TextBox txtCerca;
+    }
+}
+
---
+++ Esercizio_Maneggio/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+namespace Esercizio_Maneggio
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        bool selezione = false;
+        Cavallo[] maneggio = new Cavallo[10];
+        int i = 0;
+        List<string> Cavalli = new List<string>();
+        string percorso = @"C:\Users\alesg\OneDrive\Desktop\Maneggio.txt";
+
+        public struct Cavallo
+        {
+            public string Nome;
+            public string Razza;
+            public int Anno;
+            public char Sesso;
+
+            public Cavallo(string nome, string razza, int anno, char sesso)
+            {
+                Nome = nome;
+                Razza = razza;
+                Anno = anno;
+                Sesso = sesso;
+            }
+
+           
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            pnlCerca.Visible = false;
+        }
+
+        private void txtBNome_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+
+            if (!Char.IsLetter(e.KeyChar) && e.KeyChar != (char)8 && e.KeyChar != ' ')
+            {
+                e.KeyChar = (char)0;
+            }
+        }
+
+        private void txtBRazza_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!Char.IsLetter(e.KeyChar) && e.KeyChar != (char)8 && e.KeyChar != ' ')
+            {
+                e.KeyChar = (char)0;
+            }
+        }
+
+        private void txtBAnno_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.KeyChar = (char)0;
+            }
+        }
+
+        private void rdBM_CheckedChanged(object sender, EventArgs e)
+        {
+            selezione = true;
+        }
+
+        private void rdBF_CheckedChanged(object sender, EventArgs e)
+        {
+            selezione = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void btnSalva_Click(object sender, EventArgs e)
+        {
+            string nome = txtBNome.Text;
+
+            if (string.IsNullOrEmpty(nome))
+            {
+                MessageBox.Show("Inserisci un nome valido");
+                return;
+            }
+            string razza = txtBRazza.Text;
+            if (string.IsNullOrEmpty(razza))
+            {
+                MessageBox.Show("Inserisci una razza valida");
+                return;
+            }
+            
+            if (!int.TryParse(txtBAnno.Text, out int anno) || anno < 1995|| anno >= 2027)
+            {
+                MessageBox.Show("Inserisci un anno tra il 1900 e il 2026");
+                return;
+            }
+            char sesso = ' ';
+            if (selezione == true)
+            {
+                if (rdBM.Checked)
+                {
+                   sesso = 'M';
+                }
+                else
+                {
+                 sesso = 'F';
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Seleziona il sesso del cavallo");
+            }
+            Cavallo cavallo = new Cavallo(nome, razza, int.Parse(txtBAnno.Text), sesso);
+            string  cavalloString = $"Nome: {cavallo.Nome}, Razza: {cavallo.Razza}, Anno: {cavallo.Anno}, Sesso: {cavallo.Sesso}";
+            maneggio[i] = cavallo;
+            i++;
+           
+            lbCavalli.Items.Add(cavalloString.ToString());
+            string[] righe = {$"{cavallo.Nome} ; {cavallo.Razza} ; {cavallo.Anno} ; {cavallo.Sesso}"};
+            File.AppendAllLines(percorso, righe);
+
+            MessageBox.Show("Cavallo salvato con successo");
+            txtBAnno.Clear();
+            txtBNome.Clear();
+            txtBRazza.Clear();
+        }
+
+        private void btnRimuovi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerca_Click(object sender, EventArgs e)
+        {
+            pnlCerca.Visible = true;   
+        }
+    }
+}
